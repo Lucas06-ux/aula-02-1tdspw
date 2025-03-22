e9c2db00b9605584d9301fd66f92e6d4176da217
Lucas06-ux <lubgian@gmail.com>
2025-03-21 21:57:53 -0300

    Nova classe adicionada na aula inicial de Java e novas linhas

--- Aula01/src/OperadoresAritmeticos.java
+++ Aula01/src/OperadoresAritmeticos.java
@@ -4,12 +4,30 @@ public class OperadoresAritmeticos {
         //Declarar 3 variáveis
         float cp =10;
         float challenge =7;
-        float gs = 7;
+        float gs = 8;
 
         //Calcular a média do semestre (20% cp. 20% challenge, 60% gs)
         double media = cp * 0.2 + challenge * 0.2 + gs * 0.6;
-        System.out.print("Média " +media);
+        System.out.println("Média " +media);
 
         //  Exibir a média do semestre
+
+        int x = 10;
+        x ++; //incremento em 1 unidade ( x = x+1)
+        System.out.println(x); //x=11
+        x --; //decremento em 1 unidade (x = x-1)
+        System.out.println(x); // x = 10
+        x += 2;
+        System.out.println(x);
+        x -= 4;
+        System.out.println(x);
+        x *=3;
+        System.out.println(x);
+        x /= 4;
+        System.out.println(x);
+        x %= 2;
+        System.out.println(x);
+
+
     }
 }
